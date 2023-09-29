e6971ba388bc649ecc47b0b18428a80bc3a3e725
Chalunthorn Niyom <chalunthorn.niyom@gmail.com>
2023-09-29 18:46:55 +0700

    add cors

--- src/main/java/sit/int221/sas/sit_announcement_system_backend/config/SecurityConfig.java
+++ src/main/java/sit/int221/sas/sit_announcement_system_backend/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
-        http.csrf().disable()
+        http.cors().and().csrf().disable()
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/api/token").permitAll() ///"/api/product"ยอมหมด
                         // all other requests need to be authenticated
