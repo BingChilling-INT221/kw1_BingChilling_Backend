6b416f81d608564ad85475dc590dbc8b0249028d
Chalunthorn Niyom <chalunthorn.niyom@gmail.com>
2023-12-01 22:36:53 +0700

    ประเภท String อันนึง ไม่รู้สร้างไปทำไมเหมือนกัน

---
+++ src/main/java/sit/int221/sas/sit_announcement_system_backend/utils/TokenPayload.java
@@ -0,0 +1,13 @@
+package sit.int221.sas.sit_announcement_system_backend.utils;
+
+public class TokenPayload {
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
